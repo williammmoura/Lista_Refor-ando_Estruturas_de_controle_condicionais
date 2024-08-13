bf2912df0fdbf1b501c38dd1b1e3ab00d5ec74e8
William Moura <williamonte@gmail.com>
2024-08-13 00:03:25 -0300

    Feat: Exercício 10

--- ex_10_imc.js
+++ ex_10_imc.js
@@ -1,3 +1,14 @@
+/*10 - Calcule o IMC e determine a categoria
+
+Escreva um programa que leia o peso (kg) e a altura (m) de uma pessoa, calcule o Índice de Massa Corporal (IMC) e use uma estrutura
+if/else para determinar a categoria:
+            Abaixo do peso: IMC < 18.5
+            Peso normal: IMC 18.5 - 24.9
+            Sobrepeso: IMC 25 - 29.9
+            Obesidade grau I: IMC 30 - 34.9
+            Obesidade grau II: IMC 35 - 39.9
+            Obesidade grau III: IMC >= 40
+Dica: O IMC é calculado como peso dividido pela altura ao quadrado (IMC = peso / altura²).*/
 const prompt = require('prompt-sync')();
 
 let userWeight = Number(prompt('Enter your weight:(Kg) '));
